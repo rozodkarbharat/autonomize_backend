278ce54cee981045d2ab33f84928fbb242199c78
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2025-01-11 20:23:53 +0530

    databse url added for hosting

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -51,15 +51,98 @@ userRoute.get("/user-detail/:username", (req, res) => {
     }
 })
 
-
 userRoute.get("/get-repos/:username",(req, res) => {
     try{
         let username = req.params.username
-        let sqlQuery = `UPDATE  FROM user WHERE username =?`
+    
+        let sqlQuery = `SELECT * FROM user WHERE username=?`
+        connection.query(sqlQuery,[username],async(err,result) => {
+            if(err){
+                console.log(err.message)
+                return res.status(401).send({message:"error in fetching repositories",error:true})
+            }
+
+            if(result.length > 0 && result[0].repositories){
+                return res.status(200).send({data: JSON.parse(result[0].repositories),error:false})
+            }
+            else{
+                let userdata = JSON.parse(result[0].userdata)
+                let response= await fetch( userdata.repos_url,{
+                    headers: {
+                    Authorization: `Bearer ${process.env.token}`, // Include the token in the Authorization header
+                    Accept: "application/vnd.github.v3+json"
+                }})
+                let data = await response.json()
+                console.log(data,'userdata',username)
+                if(data.message){
+                    return res.status(404).send({message:"Repositories not found",error:true})
+                }
+                else{
+                    let sqlQuery = `UPDATE user SET repositories = ? WHERE username = ?`
+
+                    connection.query(sqlQuery,[JSON.stringify(data),username],(err,inserresult) => {
+                        if(err){
+                            console.log(err.message)
+                            return res.status(401).send({message:"error in inserting repositories details",error:true})
+                        }
+                        return res.status(200).send({data:data,error:false})
+                    })
+                }
+            }
+
+
+        })
     }
     catch(e){
 
     }
 })
+
+
+userRoute.get('/followers/:username',(req,res) => {
+    try{
+        let username = req.params.username
+        let sqlQuery = `SELECT * FROM user WHERE username=?`
+        connection.query(sqlQuery,[username],async(err,result) => {
+            if(err) {
+                console.log(err.message)
+                return res.status(401).send({message:"error in fetching followers",error:true})
+            }
+            
+            if(result.length > 0 && result[0].followers) {
+                return res.status(200).send({data: JSON.parse(result[0].followers),error:false})
+            }
+            else{
+                let userdata = JSON.parse(result[0].userdata)
+                let response= await fetch( userdata.followers_url,{
+                    headers: {
+                    Authorization: `Bearer ${process.env.token}`, // Include the token in the Authorization header
+                    Accept: "application/vnd.github.v3+json"
+                }})
+                let data = await response.json()
+                if(data.message){
+                    return res.status(404).send({message:"followers not found",error:true})
+                }
+                else{
+                    let sqlQuery = `UPDATE user SET followers = ? WHERE username = ?`
+
+                    connection.query(sqlQuery,[JSON.stringify(data),username],(err,inserresult) => {
+                        if(err){
+                            console.log(err.message)
+                            return res.status(401).send({message:"error in inserting followers details",error:true})
+                        }
+                        return res.status(200).send({data:data,error:false})
+                    })
+                }
+            }
+        })
+    }
+    catch(e){
+
+    }
+})
+
+
+
 
 module.exports = userRoute
