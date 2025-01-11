17785a965ba388a26011a98b7dfc531c80a0e396
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2025-01-11 20:50:42 +0530

    db file removed from gitignore

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const userRoute = require("./Routes/user.route");
 const app = express();
 
 app.use(express.json())
-app.use(cors({
-    credentials: true,
-}));
+app.use(cors());
 
 app.use("/user",userRoute)
 
